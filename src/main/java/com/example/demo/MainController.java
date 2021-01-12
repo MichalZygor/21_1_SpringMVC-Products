b4c0ea22bd7387c235375f63d46aaf8eff9ae460
Michał <michal@winami.com>
2021-01-12 02:24:25 +0100

    Praca nad rozwiazaniem

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,13 +1,61 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MainController {
 
+    private List<Products> products;
+
+    public MainController(){
+        products = new ArrayList<>();
+        products.add(new Products(1, "Czekoladki", 22.25, "src/czekoladki.png","Inne"));
+        products.add(new Products(2, "Chleb", 4.85, "src/chleb.png","Artykuły spożywcze"));
+        products.add(new Products(3, "Talerz 22cm", 5.20, "src/talerz.png","Artykuły gospodarstwa domowego</"));
+        products.add(new Products(4, "Młotek", 22.25, "src/mlotek.png","Inne"));
+    }
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+
+        List<String> categoriesList = new ArrayList<>();
+
+        categoriesList.add("Artykuły spożywcze");
+        categoriesList.add("Artykuły gospodarstwa domowego");
+        categoriesList.add("Inne");
+
+        model.addAttribute("categoriesList", categoriesList);
+
         return "home";
+    }
+
+
+    @GetMapping("/list")
+    public String categories(Model model){
+
+        model.addAttribute("products", products);
+        return "category";
+    }
+
+    @GetMapping("/add")
+    public String add(Model model) {
+        Products products  = new Products();
+
+        model.addAttribute("product", new Products());
+
+
+        return "add";
+    }
+
+    @PostMapping("/add")
+    public String addProduct(Products product){
+        products.add(product);
+        return "redirect:/list?category=wszystko";
     }
 }
---
+++ src/main/java/com/example/demo/Products.java
@@ -0,0 +1,60 @@
+package com.example.demo;
+
+public class Products {
+    private Integer id;
+    private String name;
+    private Double price;
+    private String imageUrl;
+    private String category;
+
+    public Products() {
+    }
+
+    public Products(Integer id, String name, Double price, String imageUrl, String category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+        this.category = category;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
