be4b7f68daa095dce6b9d64e49ea1fad3056a221
Michał <michal@winami.com>
2021-01-14 01:19:05 +0100

    solution - without addidng

--- src/main/java/com/example/demo/Categories.java
+++ src/main/java/com/example/demo/Categories.java
@@ -1,10 +1,40 @@
 package com.example.demo;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Categories {
-    ArrayList<String> name;
+    private Integer id;
+    private String name;
+    private Boolean isFictive;
 
+    public Categories() {
+    }
 
+    public Categories(Integer id, String name, Boolean isFictive) {
+        this.id = id;
+        this.name = name;
+        this.isFictive = isFictive;
+    }
+
+    public Boolean getFictive() {
+        return isFictive;
+    }
+
+    public void setFictive(Boolean fictive) {
+        isFictive = fictive;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -4,14 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 @Controller
 public class MainController {
 
     private List<Products> products;
+    private List<Categories> categories;
 
     public MainController(){
         products = new ArrayList<>();
@@ -19,35 +23,53 @@ public class MainController {
         products.add(new Products(2, "Chleb", 4.85, "src/chleb.png","Artykuły spożywcze"));
         products.add(new Products(3, "Talerz 22cm", 5.20, "src/talerz.png","Artykuły gospodarstwa domowego</"));
         products.add(new Products(4, "Młotek", 22.25, "src/mlotek.png","Inne"));
+
+        categories = new ArrayList<>();
+        categories.add(new Categories(1, "Wszystkie kategorie", true));
+        categories.add(new Categories(2, "Artykuły spożywcze", false));
+        categories.add(new Categories(3, "Artykuły gospodarstwa domowego", false));
+        categories.add(new Categories(4, "Inne", false));
     }
 
     @GetMapping("/")
     public String home(Model model) {
-
-        List<String> categoriesList = new ArrayList<>();
-
-        categoriesList.add("Artykuły spożywcze");
-        categoriesList.add("Artykuły gospodarstwa domowego");
-        categoriesList.add("Inne");
-
-        model.addAttribute("categoriesList", categoriesList);
-
+        model.addAttribute("categories", categories);
         return "home";
     }
 
 
     @GetMapping("/list")
-    public String categories(Model model){
+    public String categories(Model model, @RequestParam(name = "category") String selectedCategory){
 
-        model.addAttribute("products", products);
+        model.addAttribute("categories", categories);
+        model.addAttribute("selectedCategory", selectedCategory);
+
+        if(selectedCategory.equals("Wszystkie kategorie")){
+            model.addAttribute("products", products);
+            model.addAttribute("productsTotalSum", products.stream()
+                                                                .mapToDouble(product -> product.getPrice())
+                                                                .sum());
+        }
+        else {
+            model.addAttribute("products", products.stream()
+                                                        .filter(product -> product.getCategory().equals(selectedCategory))
+                                                        .collect(Collectors.toList()));
+            model.addAttribute("productsTotalSum", products.stream()
+                                                                .filter(product -> product.getCategory().equals(selectedCategory))
+                                                                .mapToDouble(product -> product.getPrice())
+                                                                .sum());
+        }
         return "category";
     }
 
     @GetMapping("/add")
     public String add(Model model) {
-        Products products  = new Products();
-
-        model.addAttribute("product", new Products());
+        //Products products  = new Products();
+        model.addAttribute("newProduct", new Products());
+        model.addAttribute("products", products);
+        model.addAttribute("categories", categories.stream()
+                                                        .filter(categories1 -> !categories1.getName().equals("Wszystkie kategorie"))
+                                                        .collect(Collectors.toList()));
 
 
         return "add";
